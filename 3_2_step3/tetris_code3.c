8767ca12dc76f803cb69a722d7262ef43f899c15
Koji Abe <gracehime@gmail.com>
2022-04-21 12:51:41 +0900

    【3.2 STEP2 文字を時間とともに移動させる方法】-> 【コード 3 一行ずつ下に文字を表示するプログラム】を写経し、動作を確認した。

---
+++ 3_2_step3/tetris_code3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define clearScreen()       printf("\033[2J")
+#define setPosition(x,y)    printf("\033[%d;%dH",(y),(x))
+#define setCharColor(n)     printf("\033[3%dm",(n))
+#define setBackColor(n)     printf("\033[4%dm",(n))
+
+#define BLACK   0
+#define RED     1
+#define GREEN   2
+#define YELLOW  3
+#define BLUE    4
+#define MAGENTA 5
+#define CYAN    6
+#define WHITE   7
+#define DEFAULT 9
+
+int main(int argc, char* argv[])
+{
+    int y;
+
+    clearScreen();  // 画面クリア
+    setBackColor(BLUE); // 文字色設定
+    setCharColor(YELLOW);   // 文字の背景色設定
+
+    for (y = 1; y < 23; y++)    // 1行ずつ繰り返す(23回)
+    {
+        setPosition(5, y);
+        printf("$");   fflush(stdout);
+    }
+
+    setBackColor(DEFAULT);  // 文字背景色を元に戻す
+    setCharColor(DEFAULT);  // 文字色を元に戻す
+}
